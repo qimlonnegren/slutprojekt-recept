5d9812b144b6b782de6d07c0106486a0a51786c1
Qim Lönnegren <qim.lonnegren@iths.se>
2023-01-11 10:14:51 +0100

    Add link, change Array type, map categories

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -1,19 +1,24 @@
 import styling from '../styling.module.css'
 import { useState, useEffect } from 'react'
-
+import { Link } from 'react-router-dom';
 
 const SideMenu = () => {
-    const [Categories, setCategories] = useState<Array<string>>([]);
+    const [categories, setCategories] = useState<Array<any>>([]);
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_BASE_URL}/categories`)
             .then(data => data.json())
-            .then(Categories => setCategories(Categories))
-
+            .then(data => setCategories(data))
     }, [])
-    return <div className={styling.parentSideMenuCss}><h2 className={styling.sideMenuCss}>Kategorier</h2><ul>{Categories.map(category => <li>{category} </li>)}</ul></div>
+
+    return <div className={styling.parentSideMenuCss}>
+        <h2 className={styling.sideMenuCss}>Kategorier</h2>
+        <ul>
+            {categories.map(category =>
+                <li key={category._id[0]}><Link to={ '/category/' + category._id[0] }>{category._id[0]} ({category.count})</Link></li>
+            )}
+        </ul>
+    </div>
 }
+
 export default SideMenu;
-
-
-// /categories/:categoryName/recipes'
