a103975f3080d7a8b5f14cdce46fe8aeac23b48b
Qim Lönnegren <qim.lonnegren@iths.se>
2023-01-11 10:12:31 +0100

    Remove routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import SideMenu from './components/SideMenu';
-import SingleRecipe from './components/SingleRecipe';
-// import SmallRecipeComponent from './components/SmallRecipeComponent';
-import Recipes from './components/Recipes';
-// import Title from './components/Title';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -21,30 +12,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        {/* <Route index element={<Title />} /> */}
-          <Route path=":recipes" element={<Recipes />} >
-            <Route path=":singleRecipe" element={<SingleRecipe />} />
-          </Route>
-          <Route path='*' element={
-            <div>
-              <p>Nothing to see here!</p>
-            </div>
-          } />
-        {/* </Route> */}
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
